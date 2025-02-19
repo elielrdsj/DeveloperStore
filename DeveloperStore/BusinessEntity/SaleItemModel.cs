f9c95570d2ad5066bfddde67e940960c1c1deaa0
eliel <eliel@ELIEL>
2025-02-18 21:16:46 -0300

    Adicionar arquivos de projeto.

---
+++ DeveloperStore/BusinessEntity/SaleItemModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeveloperStore.BusinessEntity
+{
+    public class SaleItem
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string ProductId { get; set; } = string.Empty; // External Identity
+
+        [Required]
+        [Range(1, 20, ErrorMessage = "Quantity must be between 1 and 20.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Unit price must be greater than 0.")]
+        public decimal UnitPrice { get; set; }
+
+        public decimal Discount => CalculateDiscount();
+
+        [NotMapped]
+        public decimal TotalPrice => (UnitPrice * Quantity) - Discount;
+
+        private decimal CalculateDiscount()
+        {
+            if (Quantity < 4) return 0;
+            if (Quantity >= 4 && Quantity < 10) return (UnitPrice * Quantity) * 0.10m;
+            if (Quantity >= 10 && Quantity <= 20) return (UnitPrice * Quantity) * 0.20m;
+            return 0;
+        }
+    }
+}
---
+++ DeveloperStore/BusinessEntity/SaleModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeveloperStoreAPI.Models
+{
+    public class Sale
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string SaleNumber { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime SaleDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string CustomerId { get; set; } = string.Empty; // External Identity
+
+        [Required]
+        public string BranchId { get; set; } = string.Empty; // External Identity
+
+        [Required]
+        public List<SaleItem> Items { get; set; } = new();
+
+        public bool IsCancelled { get; set; } = false;
+
+        [NotMapped]
+        public decimal TotalAmount => Items.Sum(item => item.TotalPrice);
+    }
+}
